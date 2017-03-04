f6fbf955be6361836a39d7bb82b8689025ea3e8c
Syo Ikeda <suicaicoca@gmail.com>
2017-03-04 10:25:33 +0900

    swift package init

---
+++ Tests/SwiftActorTests/SwiftActorTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import SwiftActor
+
+class SwiftActorTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+
+    static var allTests : [(String, (SwiftActorTests) -> () throws -> Void)] {
+        return [
+            ("testExample", testExample),
+        ]
+    }
+}
